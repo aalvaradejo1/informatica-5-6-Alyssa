6f8f8582a2f9bd606cfc9d03e3f3ceae1714b06e
Alyssa Alvaradejo <assylalva@gmail.com>
2025-08-26 16:40:04 +0000

    fizzbuzz

---
+++ checkpoint 3/large_numbers.py
@@ -0,0 +1,7 @@
+def main():
+    n1 = int(input("Number 1:"))
+    n2 = int(input("Number 2:"))
+    n3 = int(input("Number 3:"))
+    n4 = int(input("Number 4:"))
+
+    if n1 > n2 and n1 > n3 and n1
---
+++ fizzbuzz.py
@@ -0,0 +1,35 @@
+def main():
+    number = int(input("Enter an integer:"))
+
+    if number != 2 and number % 2 != 0 and number % number == 0:
+        print("Prime ")
+    elif number != 3 and number % 3 != 0 and number % number == 0:
+        print("Prime ")
+    elif number != 4 and number % 4 != 0 and number % number == 0:
+        print("Prime ")
+    elif number != 5 and number % 5 != 0 and number % number == 0:
+        print("Prime ")
+    elif number != 6 and number % 6 != 0 and number % number == 0:
+        print("Prime ")
+    elif number != 7 and number % 7 != 0 and number % number == 0:
+        print("Prime ")
+    elif number != 8 and number % 2 != 8 and number % number == 0:
+        print("Prime ")
+    elif number != 9 and number % 2 != 9 and number % number == 0:
+        print("Prime ")
+    elif number != 10 and number % 10 != 0 and number % number == 0:
+        print("Prime ")
+
+    fizzbuzz(number)
+
+def fizzbuzz(number):
+    if number % 3 == 0:
+        print("Frizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(f"{number}")
+
+main()
