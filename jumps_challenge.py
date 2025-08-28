9f0fb6ea7e81fa614b57ad744a0e0e20162d48a9
Alyssa Alvaradejo <assylalva@gmail.com>
2025-08-28 16:33:46 +0000

    challenge jumps

--- jumps_challenge.py
+++ jumps_challenge.py
@@ -2,13 +2,16 @@ def main():
     number = int(input("enter limit number: "))
     step_size = int(input("Enter the step size you want: "))
 
-    start = 1
+    start = step_size
+    
     while True:
         print(step_size)
         start += step_size
         print (start)
+        
         if start == number:
             print(number)
+        else:
             break
 
 main() 
