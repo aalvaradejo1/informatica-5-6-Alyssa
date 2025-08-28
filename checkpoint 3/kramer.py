5e04a536be92be74e52741de6e577a46d2743788
Alyssa Alvaradejo <assylalva@gmail.com>
2025-08-28 16:01:36 +0000

    kramer

--- checkpoint 3/kramer.py
+++ checkpoint 3/kramer.py
@@ -1,9 +1,9 @@
-while greeting:
+while True:
     
-greeting = input("write a greeting:")
+        greeting = input("write a greeting:")
 
-if greeting.startswith("hello"):
-        print("You get $0 dollars")
-elif greeting.startswith("h"):
-        print("You get $20 dollars")
-else: print("You get $100 dollars")
+        if greeting.startswith("hello"):
+                print("You get $0 dollars")
+        elif greeting.startswith("h"):
+                print("You get $20 dollars")
+        else: print("You get $100 dollars")
