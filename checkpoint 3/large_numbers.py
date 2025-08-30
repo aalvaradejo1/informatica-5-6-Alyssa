7aee4a32139812b3ace68a2f038230c614cd6625
Alyssa Alvaradejo <assylalva@gmail.com>
2025-08-30 04:11:47 +0000

    large_numbers

--- checkpoint 3/large_numbers.py
+++ checkpoint 3/large_numbers.py
@@ -5,4 +5,12 @@ def main():
     n4 = int(input("Number 4:"))
 
     if n1 > n2 and n1 > n3 and n1 > n4:
-        
+        greater = n1
+    elif n2 > n3 and n2 > n4:
+        greater = n2
+    elif n3 > n4:
+        greater = n3
+    else: 
+        greater = n4
+    print(f"greater number is {greater}")
+main()
