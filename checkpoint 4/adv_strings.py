e14333724aab12b6a398b33d3e79ff69a8602a3a
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-01 19:38:31 +0000

    adv strings

---
+++ checkpoint 4/adv_strings.py
@@ -0,0 +1,29 @@
+#String Length
+input_string = input("Please type a message: ")
+# print(input_string)
+
+
+# print("-" * len(input_string))
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[3])
+
+#Example
+
+# i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+#Example 2
+# i = len(input_string)-1
+# while i >= 0:
+#     print(input_string[i])
+#     i -= 1
+
+print("h" in input_string)
+print("j" in input_string)
+print("ello" in input_string)
+print(input_string.find("h"))
