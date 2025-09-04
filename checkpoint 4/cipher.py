12f52430d8d8044c9e29851ea1b7c1c7674f4c47
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-04 17:19:50 +0000

    multiplicationtables

--- checkpoint 4/cipher.py
+++ checkpoint 4/cipher.py
@@ -5,15 +5,15 @@ def main():  # define the main function
     encode_message(message)    # we execute the function
 
 def encode_message(text):       #definind the function that will encode the message
-    alphabet = "abcdefghijklmnopqrstuvwxyz"       # created a variable to store all the letters from a to z
-    cipher = "zyxwvutsrqponmlkjihgfedcba"         # created a variable to store all the letters from z to a
-    new_message = ""                # that is the variable which will be printed at the end
+    alphabet = "abcdefghijklmnopqrstuvwxyz"   # created a variable to store all the letters from a to z
+    cipher = "zyxwvutsrqponmlkjihgfedcba"   # created a variable to store all the letters from z to a
+    new_message = ""   # that is the variable which will be printed at the end
     i = 0         # this is our counter
 
-    while i < len(text):      # inicialized the loop with our counter that will update every time the loop starts again until the condition is false
-        char = text[i]              # we created a variable that stores the position 0( but will update every time) of the message the user put
+    while i < len(text): # inicialized the loop with our counter that will update every time the loop starts again until the condition is false
+        char = text[i]  # we created a variable that stores the position 0( but will update every time) of the message the user put
         
-        if char in alphabet:                   # we created a condition which states that if the character that is in the position 0  is in the alphabet it will do the following
+        if char in alphabet:   # we created a condition which states that if the character that is in the position 0  is in the alphabet it will do the following
             cipher_index = alphabet.find(char) # in this variable will be stored the position that the letter of the massage takes in the alphabet
             new_message += cipher[cipher_index] # # here we updated the new message that will be printed to heve the character that is in the same position as in the alphabet but for the cipher variable so that it is encoded
         else: new_message += char              # here we stated that if any character that doesn't cualifies for the previous condition it will just be printed or added to the variable that will be printed at the end
---
+++ checkpoint 4/multiplication.py
@@ -0,0 +1,18 @@
+def main():
+    number = int(input("Enter a positive integer number: "))
+
+    multi(number)
+
+def multi(num): 
+    m = 1
+
+    while m <= 10:
+        if num > 0:
+            
+            table = num * m
+            print(f"{num} x {m} = {table}")
+            m += 1
+            
+        else: print("try again with an integer number")
+
+main()  
