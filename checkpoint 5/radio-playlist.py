b8212f6990bf341fd19049fc4eb1ac81f2489d4d
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-17 20:07:54 +0000

    radio

--- checkpoint 5/radio-playlist.py
+++ checkpoint 5/radio-playlist.py
@@ -1,8 +1,17 @@
 def main():
     songs = ["Blinding Lights","Levitating","As It Was","Heat Waves","Good 4 u"]
     
+
+    
     songs.append("Good for u")
     songs.insert(0, "Bohemian Rhapsody")
     songs.remove("Good for u")
     print(songs.index("Levitating"))
-    print(songs.count())
+    print(songs.count("As it was"))
+    print(songs.reverse())
+    print(songs.sort())
+    print(songs)
+
+    print(songs)
+
+main()
