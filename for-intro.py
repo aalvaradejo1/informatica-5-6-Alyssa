ea9207187f31da44a6dc65426db8ea74b2dc33ad
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-17 20:20:34 +0000

    for intro

---
+++ for-intro.py
@@ -0,0 +1,14 @@
+doctrine = ["Faith", "Repentance", "Baptism", "Holy Ghost", "Endure to the end"]
+
+# i = 0
+# while i < len(doctrine)
+#     print(doctrine[i])
+#     i += 1
+
+for principle in doctrine:
+    print(principle)
+
+
+school_objective = ["Deepen convertion to Jesus Christ and His restored gospel", "Qualify for the blessings of the Temple", "Prepare for missionary and church service", "Build the kingdom of God"]
+for objective in school_objective:
+    print(o)
