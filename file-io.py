df36b59f40b20792550eddab03438a00a99ca66c
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-18 17:22:59 +0000

    file

---
+++ file-io.py
@@ -0,0 +1,14 @@
+# names = []
+
+# for i in range(3):
+#     names.append(input("What's your name? "))
+
+# for name in sorted(names):
+#     print(f"Hello {name}")
+
+name = input("What's your name? ")
+file = open("names.txt", "a")
+file.write(f"{name}\n")
+file.close()
+    
+    
