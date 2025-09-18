1518b38b6d52edf5cd099dbe932ed26fc8fc63d6
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-18 16:09:25 +0000

    radio not finished

--- checkpoint 5/radio-playlist.py
+++ checkpoint 5/radio-playlist.py
@@ -3,15 +3,20 @@ def main():
     
 
     
-    songs.append("Good for u")
+    songs.append("Drivers license")
     songs.insert(0, "Bohemian Rhapsody")
     songs.remove("Good for u")
-    print(songs.index("Levitating"))
-    print(songs.count("As it was"))
-    print(songs.reverse())
-    print(songs.sort())
+    songs.index("Levitating")
     print(songs)
+    songs.count("As it was")
+    print(songs)
+    songs.reverse()
+    print(songs)
+    songs.sort()
+    print(songs)
+    
+    playlist = songs
 
-    print(songs)
+    print(playlist)
 
 main()
