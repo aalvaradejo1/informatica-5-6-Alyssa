19fa9df011e8836d82d65fe070c5d473e44ef16d
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-24 20:20:43 +0000

    matrix operations

--- checkpoint 7/matrix-operations.py
+++ checkpoint 7/matrix-operations.py
@@ -12,10 +12,17 @@ def sum_of_column(matrix, column_number: int):
         column_sum = column_sum + row[column_number]
     return column_sum
 
+
+def change_value(matrix, row_number, column_number, new_value):
+    row = matrix[row_number]
+    row[column_number] = new_value
+
 m = [[4,2,3,2],[9,1,12,11],[7,8,9,5],[2,9,15,1]]
 # my_sum = sum_of_row(m, 1)  
-my_sum = sum_of_column(m,2)
-print(my_sum)
+# my_sum = sum_of_column(m,2)
+print(m)
+change_value(m, 2, 3, 1000)
+print(m)
 
 
         
