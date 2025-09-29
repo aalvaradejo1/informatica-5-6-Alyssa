8c2ffc9181f6c5070fa1fd84a25724e3a53dc806
Alyssa Alvaradejo <assylalva@gmail.com>
2025-09-29 19:40:02 +0000

    dictionary methodds

---
+++ checkpoint 8/dictionary-methods.py
@@ -0,0 +1,38 @@
+dictionary = {
+    "color" : "green",
+    "age" : 17
+}
+
+
+#Values
+print(dictionary.values())
+for v in dictionary.values():
+    print(v)
+
+#Keys
+print(dictionary.keys())
+for k in dictionary.keys():
+    print(k)
+
+#items
+print(dictionary.items())
+for i in dictionary.items():
+    print(i)
+
+#print Key and Value using methods
+# to do
+
+
+#Get
+print_items = {"apples": 5, "cups":2}
+print(f"I'm bringging {print_items.get("cups")} cups.")
+print(f"I'm bringging {print_items.get("eggs", 0)} eggs.")
+
+#Setting default values
+pet_info = {
+    "name" : "Puka",
+    "age" : 5
+}
+
+pet_info.setdefault("color", "black")
+print(pet_info)
