ce58e43653d1ce368192cbae8b96ae0f4ae57868
Alyssa Alvaradejo <assylalva@gmail.com>
2025-10-30 16:31:12 +0000

    fuel

--- checkpoint 10/fuel.py
+++ checkpoint 10/fuel.py
@@ -1,15 +1,33 @@
 def main():
-    fuel_left = input("Enter the fraction of how much fuel you have left: ").split("/")
-    percentage(fuel_left)
-
-def percentage(fuel_left):
-    # print(fuel_left)
-    num = int(fuel_left[0])
-    den = int(fuel_left[1])
+    percentage("Enter the fraction of how much fuel you have left: ")
     
-    percent = round(num / den * 100)
-    
-    if 1 < percent < 99:
-        print(f"You hvae {percent} % of fuel left")
 
+def percentage(promt):
+    while True:
+        try:
+            fraction = input(promt).split("/")
+            num = int(fraction[0])
+            den = int(fraction[1])
+            percent = round(num / den * 100)
+        
+            if 1 < percent < 99:
+                print(f"You have {percent} % of fuel left")
+                break
+            elif percent <= 1:
+                percent = "E"
+                print(f"Tank: {percent}")
+                break
+            elif 101 > percent >= 99:
+                percent = "F"
+                print(f"Tank: {percent}")
+                break
+            elif num > den:
+                print("Invalid, try again")
+                pass
+        except ValueError:
+            print("Invalid, try again")
+            pass
+        except ZeroDivisionError:
+            print("Invalid, try again")
+            pass
 main()
