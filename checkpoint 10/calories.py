44bfa0893cbf06f4dd92c4a9f39f5d5501b32021
Alyssa Alvaradejo <assylalva@gmail.com>
2025-10-31 17:31:04 +0000

    calories

--- checkpoint 10/calories.py
+++ checkpoint 10/calories.py
@@ -1,13 +1,33 @@
 def main():
     food_dictionary = {
-        "Milk" : 73, "Almond milk" : 30, "Yougurt" : 77, "Sour cream" : 16, "Egg" : 75, "Egg whites" : 16, "Watermelon" : 11, "Banana" : 33, "Orange" : 21, "Avocados" : 48
+        "milk" : 73, "almond milk" : 30, "yougurt" : 77, "sour cream" : 16, "egg" : 75, "egg whites" : 16, "watermelon" : 11, "banana" : 33, "orange" : 21, "avocados" : 48
     }
     print(food_dictionary)
-    food = input("Type the name of two foods from above(separate them with commas): ").split("/")
-    calories(food)
+    two_foods = input("Type the name of two foods from above(separate them with commas): ").split(",")
+    calories(two_foods, food_dictionary)
+    
 
-def calories(food):
+def calories(food, food_dictionary):
+    
     a = food[0]
     b = food[1]
+    total_calories = 0
+    while True:
+        try:
+            if a in food_dictionary and b in food_dictionary:
+                total_calories += food_dictionary[a]
+                total_calories += food_dictionary[b]
+                print(f"Calories: {total_calories}")
+                break
+            elif (a == food_dictionary[0] or food_dictionary[6]) and b == food_dictionary[0] or food_dictionary[6]:
+                raise ValueError("You CANNOT mix watermelon and milk")
+        except (ValueError, IndexError, ZeroDivisionError):
+            print("Invalid, try again")
+        
+        print(total_calories)
+
+
+
+
 
 main()
